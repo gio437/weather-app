5d12b04d2b99bdef887ccf399f6d96b2201414fd
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-01-07 16:20:31 -0500

    If applied will display city info by default in string format, will also allow you to type in a city  to see info

--- javascript.js
+++ javascript.js
@@ -1,14 +1,25 @@
 const getWeather = async function() {
     const api = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Detroit&APPID=c6d16474a22ccd0db575b5b424788ef9', {mode: 'cors'})
     console.log(api);
-    await weatherInfo(api);
+    const string = await api.json();
+    console.log(string);
+    const weather = document.querySelector('.weather');
+    weather.textContent = JSON.stringify(string);
 }
 getWeather();
 
-const weatherInfo = async function(api) {
-    const string = api.json();
-    console.log(string);
-    const weather = document.querySelector('.weather');
-    weather.textContent = string.data
+const newWeather = async function() {
+    const connect = document.querySelector('.submit');
+    connect.addEventListener('click', async () => {
+        const text = document.querySelector('.text');
+        let key = text.value;
+        const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${key}&APPID=c6d16474a22ccd0db575b5b424788ef9`, {mode: 'cors'})
+        console.log(api);
+        const string = await api.json();
+        console.log(string);
+        const weather = document.querySelector('.weather');
+        weather.textContent = JSON.stringify(string);
+    })
 }
+newWeather();
 
