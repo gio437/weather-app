6adf4de89d81c86447fa0b454a24d497d408b83b
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-01-08 20:59:43 -0500

    If applied will convert the temps from kelvin to imperial, will also display a error message if user connection fails

--- javascript.js
+++ javascript.js
@@ -1,45 +1,7 @@
+let err = 0;
 const getWeather = async function() {
-    const api = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Detroit&APPID=c6d16474a22ccd0db575b5b424788ef9', {mode: 'cors'})
-    console.log(api);
-    const string = await api.json();
-    console.log(string);
-    const weather = document.querySelector('.weather');
-    const area = document.createElement('div');
-    const temp = document.createElement('div');
-    const conditions = document.createElement('div');
-    const high = document.createElement('div');
-    area.id = 'area';
-    temp.id = 'temp';
-    conditions.id = 'conditions';
-    high.id = 'range';
-    const place = JSON.stringify(string.name);
-    const type = JSON.stringify(string.weather[0].description);
-    area.textContent = place.replace(/['"]+/g, '');
-    temp.textContent = JSON.stringify(string.main.temp) + '°';
-    conditions.textContent = type.replace(/['"]+/g, '');
-    high.textContent = 'H:' + JSON.stringify(string.main.temp_max) + ' L:' + JSON.stringify(string.main.temp_min);;
-    weather.appendChild(area);
-    weather.appendChild(temp);
-    weather.appendChild(conditions);
-    weather.appendChild(high);
-}
-getWeather();
-
-const newWeather = async function() {
-    const connect = document.querySelector('.submit');
-    connect.addEventListener('click', async () => {
-        const areaEl = document.getElementById('area');
-        const tempEl = document.getElementById('temp');
-        const conditionsEl = document.getElementById('conditions');
-        const highEl = document.getElementById('range');
-        areaEl.remove();
-        tempEl.remove();
-        conditionsEl.remove();
-        highEl.remove();
-
-        const text = document.querySelector('.text');
-        let key = text.value;
-        const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${key}&APPID=c6d16474a22ccd0db575b5b424788ef9`, {mode: 'cors'})
+    try {
+        const api = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Detroit&units=imperial&APPID=c6d16474a22ccd0db575b5b424788ef9', {mode: 'cors'})
         console.log(api);
         const string = await api.json();
         console.log(string);
@@ -54,15 +16,71 @@ const newWeather = async function() {
         high.id = 'range';
         const place = JSON.stringify(string.name);
         const type = JSON.stringify(string.weather[0].description);
+        const highStr = JSON.stringify(Math.round(string.main.temp_max)) + '°';
+        const lowStr = JSON.stringify(Math.round(string.main.temp_min)) + '°'
         area.textContent = place.replace(/['"]+/g, '');
-        temp.textContent = JSON.stringify(string.main.temp) + '°';
+        temp.textContent = JSON.stringify(Math.round(string.main.temp)) + '°';
         conditions.textContent = type.replace(/['"]+/g, '');
-        high.textContent = 'H:' + JSON.stringify(string.main.temp_max) + ' L:' + JSON.stringify(string.main.temp_min);;
+        high.textContent = 'H:' + highStr.replace(/['"]+/g, '') + ' L:' + lowStr.replace(/['"]+/g, '');
         weather.appendChild(area);
         weather.appendChild(temp);
         weather.appendChild(conditions);
         weather.appendChild(high);
+    }
+    catch {
+        const weather = document.querySelector('.weather');
+        const error = document.createElement('div');
+        error.id = 'error';
+        error.textContent = 'Error, Check Connection And Refresh Page.';
+        weather.appendChild(error);
+    }
+}
+getWeather();
+
+const newWeather = function() {
+    const connect = document.querySelector('.submit');
+    connect.addEventListener('click', async () => {
+        try {
+            const areaEl = document.getElementById('area');
+            const tempEl = document.getElementById('temp');
+            const conditionsEl = document.getElementById('conditions');
+            const highEl = document.getElementById('range');
+            areaEl.remove();
+            tempEl.remove();
+            conditionsEl.remove();
+            highEl.remove();
+
+            let text = document.querySelector('.text');
+            let key = text.value;
+            const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${key}&units=imperial&APPID=c6d16474a22ccd0db575b5b424788ef9`, {mode: 'cors'})
+            console.log(api);
+            const string = await api.json();
+            console.log(string);
+            const weather = document.querySelector('.weather');
+            const area = document.createElement('div');
+            const temp = document.createElement('div');
+            const conditions = document.createElement('div');
+            const high = document.createElement('div');
+            area.id = 'area';
+            temp.id = 'temp';
+            conditions.id = 'conditions';
+            high.id = 'range';
+            const place = JSON.stringify(string.name);
+            const type = JSON.stringify(string.weather[0].description);
+            const highStr = JSON.stringify(Math.round(string.main.temp_max)) + '°';
+            const lowStr = JSON.stringify(Math.round(string.main.temp_min)) + '°'
+            area.textContent = place.replace(/['"]+/g, '');
+            temp.textContent = JSON.stringify(Math.round(string.main.temp)) + '°';
+            conditions.textContent = type.replace(/['"]+/g, '');
+            high.textContent = 'H:' + highStr.replace(/['"]+/g, '') + ' L:' + lowStr.replace(/['"]+/g, '');
+            weather.appendChild(area);
+            weather.appendChild(temp);
+            weather.appendChild(conditions);
+            weather.appendChild(high);
+        }
+        catch {
+            getWeather();
+        }
     })
 }
 newWeather();
-
